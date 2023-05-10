81670489c310709d6a9a944dd5d10d7a7e248d3c
Kevin OShaughnessy <kevinoshaughnessy@ymail.com>
2023-05-10 18:25:52 -0400

    adding first event listener

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 console.log("JS is working")
 
 fetch("https://api.artic.edu/api/v1/artworks")
-.then(resposne => response.json())
+.then(response => response.json())
+
+
+click.addEventListener("click", eventHandler)
