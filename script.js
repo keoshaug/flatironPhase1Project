11b40c8f6f6e7ba295b9ffdfb585ae4096c52a32
Kevin OShaughnessy <kevinoshaughnessy@ymail.com>
2023-05-12 08:59:38 -0400

    adding renderImages to script

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 JS retrieval info:
 
 Artist:	"artist_titles"
+Medium: "artwork_type_title"
 Title:	"title"
 Category:	"category_titles"
 Style:	"style_title"
@@ -10,10 +11,18 @@ Year:		"date_display"
 
 */
 
+const renderImages = (dataArray) => {
+        console.log(dataArray)
+        dataArray.forEach(imageInfo => {
+            console.log(imageInfo)
+        })
+    }
+
+
 fetch("https://api.artic.edu/api/v1/artworks")
-.then(response => response.json());
+.then(response => response.json())
+.then(data => renderImages(data.data))
 
+const click = document.querySelector("#click")
 
-const click = document.querySelector("#click");
-
-click.addEventListener("click", eventHandler);
+//click.addEventListener("click", eventHandler)
