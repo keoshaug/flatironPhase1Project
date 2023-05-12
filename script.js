f773d3140b8a3b92f2f7e47cc2f82f13e3efc625
Kevin OShaughnessy <kevinoshaughnessy@ymail.com>
2023-05-12 06:53:36 -0400

    adding js retrieval info in notes

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-console.log("JS is working");
+/*
+
+JS retrieval info:
+
+Artist:	"artist_titles"
+Title:	"title"
+Category:	"category_titles"
+Style:	"style_title"
+Year:		"date_display"
+
+*/
 
 fetch("https://api.artic.edu/api/v1/artworks")
 .then(response => response.json());
